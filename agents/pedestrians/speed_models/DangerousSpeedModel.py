fad7ff2559cebd7211108322060e047fa781e6c6
unknown <carla@PARTICLE.ad.crm.ucsc.edu>
2022-04-25 15:15:39 -0700

    mad speed

--- agents/pedestrians/speed_models/DangerousSpeedModel.py
+++ agents/pedestrians/speed_models/DangerousSpeedModel.py
@@ -3,8 +3,8 @@ from .SpeedModel import SpeedModel
 class DangerousSpeedModel(SpeedModel):
     
     def initialize(self):
-        self._desiredSpeed = self.internalFactors["desired_speed"] * 1.6 * 20
-        self._minSpeed = self.internalFactors["min_crossing_speed"] * 1 *10
-        self._maxSpeed = self.internalFactors["max_crossing_speed"] * 1.8 * 30
-        self._relaxationTime = self.internalFactors["relaxation_time"] * 2 * 0.1
+        self._desiredSpeed = self.internalFactors["desired_speed"] * 1
+        self._minSpeed = self.internalFactors["min_crossing_speed"] * 1 
+        self._maxSpeed = self.internalFactors["max_crossing_speed"] * 1
+        self._relaxationTime = self.internalFactors["relaxation_time"] * 1
         pass
